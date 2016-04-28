861abf7d6328c637eba4e3e0052372b46c0c733d
bahuzz <bahuzz@mail.ru>
2016-04-28 14:01:42 +0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,48 @@
+$(document).ready(function() {
+	$('.product-slider').bxSlider({
+	  minSlides: 2,
+	  maxSlides: 5,
+	  slideWidth: 163,
+	  slideMargin: 0,
+	  pager: false
+	});
+
+	$('.product-slider-yel').bxSlider({
+	  minSlides: 2,
+	  maxSlides: 5,
+	  slideWidth: 163,
+	  slideMargin: 0,
+	  pager: false
+	});
+
+	$('.product-slider-rec').bxSlider({
+	  minSlides: 2,
+	  maxSlides: 5,
+	  slideWidth: 163,
+	  slideMargin: 0,
+	  pager: false
+	});
+
+	$('.public-slider').bxSlider({
+	  slideMargin: 0,
+	  pager: false
+	});
+
+	$('.news-slider').bxSlider({
+	  slideMargin: 0,
+	  pager: false
+	});
+
+	$('.mainslider').bxSlider({
+	  slideMargin: 0,
+	  slideWidth: 810
+	});
+
+		$('.utvar-slider').bxSlider({
+	  minSlides: 2,
+	  maxSlides: 5,
+	  slideWidth: 163,
+	  slideMargin: 0,
+	  pager: false
+	});
+});
